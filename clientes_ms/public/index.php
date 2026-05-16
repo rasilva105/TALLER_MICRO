7573a43dbbf9e7729ae44e33848391f472dcd2d4
Sala D17 <CSalaD17@uniboyaca.edu.co>
2026-05-16 11:38:36 -0500

    NEW STRUCTURE

--- clientes_ms/public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/Middlewares/CorsMiddleware.php';
-CorsMiddleware::handle();
-require_once __DIR__ . '/../app/Clientes/Presentation/Routers/endpoints.php';
--- reservas_ms/public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/Middlewares/CorsMiddleware.php';
-CorsMiddleware::handle();
-require_once __DIR__ . '/../app/Reservas/Presentation/Routers/endpoints.php';
